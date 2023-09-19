5e81fe5b08f3486e658440ebd7095754d9c8cf29
iqbaluddin <miqbaluddin885@gmail.com>
2023-09-19 23:23:35 +0700

    post pertama

---
+++ script.js
@@ -0,0 +1,18 @@
+let nav = document.querySelector('nav');
+let burger = document.querySelector('.toggle');
+let active = document.querySelector('ul');
+burger.addEventListener('click', function () {
+    active.classList.toggle ('active');
+    nav.classList.toggle ('active');
+});
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+    if (document.documentElement.scrollTop > 10) {
+      nav.style.background = ('#fff');
+      nav.style.boxShadow = '0px 0 5px rgba(0, 0, 0, .2)';
+    } else {
+        nav.style.background = ('');
+        nav.style.boxShadow = '';
+    }
+  } 
